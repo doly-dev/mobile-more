4fe3141b1e01228914ea0fbc32f4ec6f0f9f4ec0
蔡金锋 <jeff.cai@99bill.com>
2022-03-28 18:42:35 +0800

    refactor: 优化BizFormItemInput，支持获取InputRef；密码框切换显示时自动获取焦点；

--- src/components/BizForm/ItemInput/SuperInput.tsx
+++ src/components/BizForm/ItemInput/SuperInput.tsx
@@ -10,161 +10,203 @@ import {
   normalizeNotWhiteSpace,
   normalizeNumber
 } from '../utils/normalize';
+import { prefixClass } from '../../../config/prefixClass';
+import './SuperInput.less';
 
-export type { InputProps };
+const prefixCls = `${prefixClass}-input`;
+
+export type { InputProps, InputRef };
 
 export interface SuperInputProps extends Omit<InputProps, 'type'> {
   type?: InputProps['type'] | 'mobile' | 'bankCard' | 'idCard' | 'number';
   disabledWhiteSpace?: boolean;
   precision?: number;
+  prefix?: React.ReactNode;
+  suffix?: React.ReactNode;
 }
 
-const SuperInput: React.FC<SuperInputProps> = ({
-  type,
-  disabledWhiteSpace,
-  onBlur,
-  max = Number.MAX_SAFE_INTEGER,
-  min = Number.MIN_SAFE_INTEGER,
-  precision,
-  inputMode,
-  ...restProps
-}) => {
-  const ref = React.useRef<InputRef>(null);
-  const [state, setState] = useControllableValue<string>(restProps);
-  const needAdjustPos = React.useMemo(
-    () =>
-      type === 'mobile' ||
-      type === 'bankCard' ||
-      type === 'idCard' ||
-      type === 'number' ||
+const SuperInput = React.forwardRef<InputRef, SuperInputProps>(
+  (
+    {
+      type,
       disabledWhiteSpace,
-    [disabledWhiteSpace, type]
-  );
-  const realType = React.useMemo(() => {
-    if (type === 'mobile' || type === 'bankCard' || type === 'idCard' || type === 'number') {
-      return 'text';
-    }
-    return type;
-  }, [type]);
-  const realInputMode = React.useMemo(() => {
-    if (!inputMode && type === 'number') {
-      return 'decimal';
-    }
-    return inputMode;
-  }, [type, inputMode]);
-
-  const calcPosOpts = React.useMemo(() => {
-    const ret: Record<string, any> = {};
-    if (type === 'bankCard') {
-      ret.type = 'bankCard';
-    } else if (type === 'mobile') {
-      ret.type = 'mobile';
-    } else if (type === 'idCard') {
-      ret.maskReg = /[^\dx]/gi;
-      ret.placeholderChars = [];
-    } else if (type === 'number') {
-      ret.maskReg = /[^\d\\.-]/g;
-      ret.placeholderChars = [];
-    } else if (disabledWhiteSpace) {
-      ret.maskReg = /\s/g;
-      ret.placeholderChars = [];
-    }
-    return ret;
-  }, [type, disabledWhiteSpace]);
-
-  const normalize = React.useCallback(
-    (val) => {
-      let newValue = val;
-      if (type === 'mobile') {
-        newValue = normalizeMobile(val);
-      } else if (type === 'bankCard') {
-        newValue = normalizeBankCard(val);
-      } else if (type === 'idCard') {
-        newValue = normalizeIdCard(val);
-      } else if (type === 'number') {
-        newValue = normalizeNumber(val);
-      } else if (disabledWhiteSpace) {
-        newValue = normalizeNotWhiteSpace(val);
-      }
-      return newValue;
+      prefix,
+      suffix,
+      onBlur,
+      max = Number.MAX_SAFE_INTEGER,
+      min = Number.MIN_SAFE_INTEGER,
+      precision,
+      inputMode,
+      ...restProps
     },
-    [disabledWhiteSpace, type]
-  );
+    ref
+  ) => {
+    const internalRef = React.useRef<InputRef>(null);
+    const [state, setState] = useControllableValue<string>(restProps);
+    const needAdjustPos = React.useMemo(
+      () =>
+        type === 'mobile' ||
+        type === 'bankCard' ||
+        type === 'idCard' ||
+        type === 'number' ||
+        disabledWhiteSpace,
+      [disabledWhiteSpace, type]
+    );
+    const realType = React.useMemo(() => {
+      if (type === 'mobile' || type === 'bankCard' || type === 'idCard' || type === 'number') {
+        return 'text';
+      }
+      return type;
+    }, [type]);
+    const realInputMode = React.useMemo(() => {
+      if (!inputMode && type === 'number') {
+        return 'decimal';
+      }
+      return inputMode;
+    }, [type, inputMode]);
 
-  const handleChange = React.useCallback(
-    (val: string) => {
-      const inputElement = ref.current?.nativeElement;
-      const prevPos = ref.current?.nativeElement?.selectionEnd;
-      const nextCtrlValue = normalize(val);
-      setState(nextCtrlValue);
+    const calcPosOpts = React.useMemo(() => {
+      const ret: Record<string, any> = {};
+      if (type === 'bankCard') {
+        ret.type = 'bankCard';
+      } else if (type === 'mobile') {
+        ret.type = 'mobile';
+      } else if (type === 'idCard') {
+        ret.maskReg = /[^\dx]/gi;
+        ret.placeholderChars = [];
+      } else if (type === 'number') {
+        ret.maskReg = /[^\d\\.-]/g;
+        ret.placeholderChars = [];
+      } else if (disabledWhiteSpace) {
+        ret.maskReg = /\s/g;
+        ret.placeholderChars = [];
+      }
+      return ret;
+    }, [type, disabledWhiteSpace]);
 
-      if (inputElement && needAdjustPos) {
-        const adjustPos = calculateCursorPosition(
-          prevPos as number,
-          state,
-          val,
-          nextCtrlValue,
-          calcPosOpts
-        );
+    const normalize = React.useCallback(
+      (val) => {
+        let newValue = val;
+        if (type === 'mobile') {
+          newValue = normalizeMobile(val);
+        } else if (type === 'bankCard') {
+          newValue = normalizeBankCard(val);
+        } else if (type === 'idCard') {
+          newValue = normalizeIdCard(val);
+        } else if (type === 'number') {
+          newValue = normalizeNumber(val);
+        } else if (disabledWhiteSpace) {
+          newValue = normalizeNotWhiteSpace(val);
+        }
+        return newValue;
+      },
+      [disabledWhiteSpace, type]
+    );
 
-        if (inputElement) {
-          if (val !== nextCtrlValue) {
-            setTimeout(() => {
+    const handleChange = React.useCallback(
+      (val: string) => {
+        const inputElement = internalRef.current?.nativeElement;
+        const prevPos = internalRef.current?.nativeElement?.selectionEnd;
+        const nextCtrlValue = normalize(val);
+        setState(nextCtrlValue);
+
+        if (inputElement && needAdjustPos) {
+          const adjustPos = calculateCursorPosition(
+            prevPos as number,
+            state,
+            val,
+            nextCtrlValue,
+            calcPosOpts
+          );
+
+          if (inputElement) {
+            if (val !== nextCtrlValue) {
+              window.setTimeout(() => {
+                inputElement.selectionStart = inputElement.selectionEnd = adjustPos;
+              });
+            } else {
               inputElement.selectionStart = inputElement.selectionEnd = adjustPos;
-            });
-          } else {
-            inputElement.selectionStart = inputElement.selectionEnd = adjustPos;
+            }
           }
         }
-      }
-    },
-    [normalize, setState, needAdjustPos, state, calcPosOpts]
-  );
+      },
+      [normalize, setState, needAdjustPos, state, calcPosOpts]
+    );
 
-  // 处理最大最小值，以及精度
-  const handelBlur = React.useCallback(
-    (e: React.FocusEvent<HTMLInputElement>) => {
-      if (type === 'number' && state) {
-        let numberValue = Number(state);
-        if (numberValue > max) {
-          numberValue = max;
-        } else if (numberValue < min) {
-          numberValue = min;
+    // 处理最大最小值，以及精度
+    const handelBlur = React.useCallback(
+      (e: React.FocusEvent<HTMLInputElement>) => {
+        if (type === 'number' && state) {
+          let numberValue = Number(state);
+          if (numberValue > max) {
+            numberValue = max;
+          } else if (numberValue < min) {
+            numberValue = min;
+          }
+          const newValue =
+            typeof precision === 'number' && precision >= 0
+              ? numberValue.toFixed(precision)
+              : String(numberValue);
+          if (newValue !== state) {
+            setState(newValue);
+          }
         }
-        const newValue =
-          typeof precision === 'number' && precision >= 0
-            ? numberValue.toFixed(precision)
-            : String(numberValue);
+        onBlur?.(e);
+      },
+      [type, onBlur, state, max, min, precision, setState]
+    );
+
+    const triggerFocus = React.useCallback(() => {
+      const inputEl = internalRef.current?.nativeElement;
+
+      if (inputEl) {
+        const startPos = inputEl.selectionStart;
+        const endPos = inputEl.selectionEnd;
+        inputEl.focus();
+        window.setTimeout(() => {
+          inputEl.setSelectionRange(startPos, endPos);
+        });
+      }
+    }, []);
+
+    React.useImperativeHandle(
+      ref,
+      () => ({
+        // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+        ...internalRef.current!,
+        focus: triggerFocus
+      }),
+      [internalRef, triggerFocus]
+    );
+
+    React.useEffect(() => {
+      // 处理直接通过 form.setFieldsValue 或 initialValues
+      if (state && needAdjustPos) {
+        const newValue = normalize(state);
         if (newValue !== state) {
-          setState(newValue);
+          handleChange(newValue);
         }
       }
-      onBlur?.(e);
-    },
-    [type, onBlur, state, max, min, precision, setState]
-  );
+    }, [handleChange, needAdjustPos, normalize, type, state]);
 
-  React.useEffect(() => {
-    // 处理直接通过 form.setFieldsValue 或 initialValues
-    if (state && needAdjustPos) {
-      const newValue = normalize(state);
-      if (newValue !== state) {
-        handleChange(newValue);
-      }
-    }
-  }, [handleChange, needAdjustPos, normalize, type, state]);
+    return (
+      <div className={`${prefixCls}`}>
+        {prefix && <div className={`${prefixCls}-prefix`}>{prefix}</div>}
+        <Input
+          inputMode={realInputMode}
+          ref={internalRef}
+          {...restProps}
+          type={realType}
+          value={state}
+          onChange={handleChange}
+          onBlur={handelBlur}
+        />
+        {suffix && <div className={`${prefixCls}-suffix`}>{suffix}</div>}
+      </div>
+    );
+  }
+);
 
-  return (
-    <Input
-      inputMode={realInputMode}
-      ref={ref}
-      {...restProps}
-      type={realType}
-      value={state}
-      onChange={handleChange}
-      onBlur={handelBlur}
-    />
-  );
-};
+SuperInput.displayName = 'SuperInput';
 
 export default SuperInput;
--- src/components/BizForm/ItemInput/index.tsx
+++ src/components/BizForm/ItemInput/index.tsx
@@ -1,14 +1,11 @@
 import * as React from 'react';
 import { isMobile, isBankCard, isIdCard } from 'util-helpers';
 import { Eye, EyeSlash } from 'doly-icons';
-import classnames from 'classnames';
 import BizFormItem, { BizFormItemProps } from '../FormItem';
 import getLabel from '../utils/getLabel';
-import Input, { InputProps, SuperInputProps } from './SuperInput';
-import { prefixClass } from '../../../config/prefixClass';
-import './index.less';
+import Input, { SuperInputProps, InputRef } from './SuperInput';
 
-const prefixCls = `${prefixClass}-form-item-input`;
+export type { InputRef };
 
 export interface BizFormItemInputProps
   extends Omit<BizFormItemProps, 'children'>,
@@ -23,137 +20,143 @@ export interface BizFormItemInputProps
       | 'precision'
       | 'disabledWhiteSpace'
     > {
-  inputProps?: InputProps;
+  inputProps?: SuperInputProps;
 
   // only type password
   visibilityToggle?: boolean; // 是否显示密码切换按钮
   iconRender?: (visibilityPassword: boolean) => React.ReactNode; // 自定义图标渲染
 }
 
-const BizFormItemInput: React.FC<BizFormItemInputProps> = ({
-  type: outType,
-  maxLength,
-  max,
-  min,
-  disabledWhiteSpace,
-  precision,
-  clearable,
-  placeholder = '请输入',
-  inputProps,
-  visibilityToggle = true,
-  iconRender,
-  extra: outExtra,
-  className,
-  required,
-  ...restProps
-}) => {
-  const label = getLabel(restProps);
-  const mergeType = React.useMemo(() => {
-    return outType || inputProps?.type || 'text';
-  }, [inputProps?.type, outType]);
+const BizFormItemInput = React.forwardRef<InputRef, BizFormItemInputProps>(
+  (
+    {
+      type: outType,
+      maxLength,
+      max,
+      min,
+      disabledWhiteSpace,
+      precision,
+      clearable,
+      placeholder = '请输入',
+      inputProps,
+      visibilityToggle = true,
+      iconRender,
+      required,
+      ...restProps
+    },
+    ref
+  ) => {
+    const inputRef = React.useRef<InputRef>(null);
+    const label = getLabel(restProps);
+    const mergeType = React.useMemo(() => {
+      return outType || inputProps?.type || 'text';
+    }, [inputProps?.type, outType]);
 
-  const hasSpecialType = React.useMemo(
-    () =>
-      mergeType === 'bankCard' ||
-      mergeType === 'mobile' ||
-      mergeType === 'idCard' ||
-      mergeType === 'number',
-    [mergeType]
-  );
+    const hasSpecialType = React.useMemo(
+      () =>
+        mergeType === 'bankCard' ||
+        mergeType === 'mobile' ||
+        mergeType === 'idCard' ||
+        mergeType === 'number',
+      [mergeType]
+    );
 
-  // only type password
-  const [visibilityPassword, setVisibilityPassword] = React.useState(false); // 密码是否可见
-  const extraIcon = React.useMemo(() => {
-    if (mergeType !== 'password' || !visibilityToggle) {
-      return null;
-    }
-    if (typeof iconRender === 'function') {
-      return iconRender(visibilityPassword);
-    }
-    return visibilityPassword ? <Eye /> : <EyeSlash />;
-  }, [iconRender, mergeType, visibilityPassword, visibilityToggle]);
-  const extra = React.useMemo(() => {
-    if (mergeType === 'password' && visibilityToggle) {
+    // only type password
+    const [visibilityPassword, setVisibilityPassword] = React.useState(false); // 密码是否可见
+    const extraIcon = React.useMemo(() => {
+      if (mergeType !== 'password' || !visibilityToggle) {
+        return null;
+      }
+      let iconView: React.ReactNode;
+      if (typeof iconRender === 'function') {
+        iconView = iconRender(visibilityPassword);
+      } else {
+        iconView = visibilityPassword ? <Eye /> : <EyeSlash />;
+      }
       return (
-        <div className={`${prefixCls}-extra`}>
-          <div
-            className={`${prefixCls}-extra-icon`}
-            onClick={() => setVisibilityPassword((v) => !v)}
-          >
-            {extraIcon}
-          </div>
-          <div className={`${prefixCls}-extra-icon`}>{outExtra}</div>
+        <div
+          style={{ color: 'var(--adm-color-weak, "#999")', cursor: 'pointer' }}
+          onClick={() => {
+            setVisibilityPassword((v) => !v);
+            inputRef.current?.focus();
+          }}
+        >
+          {iconView}
         </div>
       );
-    }
-    return outExtra;
-  }, [extraIcon, outExtra, mergeType, visibilityToggle]);
+    }, [iconRender, mergeType, visibilityPassword, visibilityToggle]);
 
-  const transform = React.useCallback(
-    (value: string) => {
-      if ((mergeType === 'bankCard' || mergeType === 'mobile') && value) {
-        return value.replace(/\D/g, '');
-      }
-      return value;
-    },
-    [mergeType]
-  );
-  const type = React.useMemo(() => {
-    if (mergeType === 'password') {
-      return !visibilityPassword ? 'password' : 'text';
-    }
-    return mergeType;
-  }, [mergeType, visibilityPassword]);
-
-  return (
-    <BizFormItem
-      required={required}
-      rules={[
-        {
-          required,
-          message: `请输入${label}`
-        },
-        {
-          validator(_, value) {
-            let errMsg = '';
-            if (value) {
-              if (
-                (outType === 'mobile' && !isMobile(value)) ||
-                (outType === 'bankCard' && !isBankCard(value, { loose: true })) ||
-                (outType === 'idCard' && !isIdCard(value))
-              ) {
-                errMsg = `请输入正确的${label}`;
-              }
-            }
-
-            if (errMsg) {
-              return Promise.reject(errMsg);
-            }
-
-            return Promise.resolve();
-          },
-          transform
+    const transform = React.useCallback(
+      (value: string) => {
+        if ((mergeType === 'bankCard' || mergeType === 'mobile') && value) {
+          return value.replace(/\D/g, '');
         }
-      ]}
-      transform={transform}
-      extra={extra}
-      className={classnames(prefixCls, className)}
-      validateTrigger={hasSpecialType ? 'onBlur' : 'onChange'}
-      {...restProps}
-    >
-      <Input
-        placeholder={placeholder}
-        clearable={clearable}
-        disabledWhiteSpace={disabledWhiteSpace}
-        precision={precision}
-        maxLength={maxLength}
-        max={max}
-        min={min}
-        {...inputProps}
-        type={type}
-      />
-    </BizFormItem>
-  );
-};
+        return value;
+      },
+      [mergeType]
+    );
+    const type = React.useMemo(() => {
+      if (mergeType === 'password') {
+        return !visibilityPassword ? 'password' : 'text';
+      }
+      return mergeType;
+    }, [mergeType, visibilityPassword]);
+
+    // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+    React.useImperativeHandle(ref, () => inputRef.current!, [inputRef]);
+
+    return (
+      <BizFormItem
+        required={required}
+        rules={[
+          {
+            required,
+            message: `请输入${label}`
+          },
+          {
+            validator(_, value) {
+              let errMsg = '';
+              if (value) {
+                if (
+                  (outType === 'mobile' && !isMobile(value)) ||
+                  (outType === 'bankCard' && !isBankCard(value, { loose: true })) ||
+                  (outType === 'idCard' && !isIdCard(value))
+                ) {
+                  errMsg = `请输入正确的${label}`;
+                }
+              }
+
+              if (errMsg) {
+                return Promise.reject(errMsg);
+              }
+
+              return Promise.resolve();
+            },
+            transform
+          }
+        ]}
+        transform={transform}
+        validateTrigger={hasSpecialType ? 'onBlur' : 'onChange'}
+        {...restProps}
+      >
+        <Input
+          placeholder={placeholder}
+          clearable={clearable}
+          disabledWhiteSpace={disabledWhiteSpace}
+          precision={precision}
+          maxLength={maxLength}
+          max={max}
+          min={min}
+          suffix={extraIcon}
+          {...inputProps}
+          ref={inputRef}
+          type={type}
+        />
+      </BizFormItem>
+    );
+  }
+);
+
+BizFormItemInput.displayName = 'BizFormItemInput';
 
 export default BizFormItemInput;
