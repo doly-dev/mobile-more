4ea48bdcce1e4f6582810c1a3676d0002c72f086
caijf <369756941@qq.com>
2023-08-25 11:18:19 +0800

    docs: areaCode示例调整

--- src/components/BizForm/demos/area-code.tsx
+++ src/components/BizForm/demos/area-code.tsx
@@ -2,7 +2,6 @@ import * as React from 'react';
 import { getPCA, getPC } from 'lcn';
 import { BizFormItemAreaCode } from 'mobile-more';
 import DemoForm from './components/DemoForm';
-import options from './constants-options';
 
 const pca = getPCA({ fieldNames: { code: 'value', name: 'label' }, inland: true });
 const pc = getPC({ fieldNames: { code: 'value', name: 'label' }, inland: true });
@@ -11,21 +10,20 @@ function Demo() {
   return (
     <DemoForm
       initialValues={{
-        areaCode4: '140524',
-        areaCode5: '441900',
-        areaCode6: '140524',
-        areaCode7: '441900'
+        areaCode3: '140524',
+        areaCode4: '441900',
+        areaCode5: '140524',
+        areaCode6: '441900'
       }}
     >
-      <BizFormItemAreaCode name="areaCode1" label="地区码" options={options} />
-      <BizFormItemAreaCode name="areaCode2" label="省市区" options={pca} title="请选择省市区" />
-      <BizFormItemAreaCode name="areaCode3" label="省市" options={pc} />
-      <BizFormItemAreaCode name="areaCode4" label="省市区" options={pca} />
-      <BizFormItemAreaCode name="areaCode5" label="不设区的市" options={pca} />
-      <BizFormItemAreaCode name="areaCode6" label="只读" options={pca} readOnly />
-      <BizFormItemAreaCode name="areaCode7" label="禁用" options={pca} disabled />
+      <BizFormItemAreaCode name="areaCode1" label="省市区" options={pca} title="请选择省市区" />
+      <BizFormItemAreaCode name="areaCode2" label="省市" options={pc} />
+      <BizFormItemAreaCode name="areaCode3" label="省市区" options={pca} />
+      <BizFormItemAreaCode name="areaCode4" label="不设区的市" options={pca} />
+      <BizFormItemAreaCode name="areaCode5" label="只读" options={pca} readOnly />
+      <BizFormItemAreaCode name="areaCode6" label="禁用" options={pca} disabled />
       {/* <BizFormItemAreaCode
-        name='areaCode8'
+        name='areaCode7'
         label='自定义校验'
         options={pca}
         rules={[
