99b6b854743454660b4f6242bd97f6ef3a7e9d89
caijf <369756941@qq.com>
2023-09-15 10:21:55 +0800

    chore: picker增加单列示例

--- src/components/BizForm/demos/picker.tsx
+++ src/components/BizForm/demos/picker.tsx
@@ -7,20 +7,21 @@ function Demo() {
   return (
     <DemoForm
       initialValues={{
-        picker2: ['Fri', 'pm'],
-        picker3: ['Fri', 'pm']
+        picker3: ['Fri', 'pm'],
+        picker4: ['Fri', 'pm']
       }}
     >
       <BizFormItemPicker name="picker1" label="选择" columns={basicColumns} />
-      <BizFormItemPicker name="picker2" label="只读" columns={basicColumns} readOnly />
-      <BizFormItemPicker name="picker3" label="禁用" columns={basicColumns} disabled />
+      <BizFormItemPicker name="picker2" label="单列" columns={basicColumns.slice(0, 1)} />
+      <BizFormItemPicker name="picker3" label="只读" columns={basicColumns} readOnly />
+      <BizFormItemPicker name="picker4" label="禁用" columns={basicColumns} disabled />
       <BizFormItemPicker
         names={['week', 'time']}
         label="字段解构"
         columns={basicColumns}
         help="设置 names 将自动解构到对应字段名中"
       />
-      <BizFormItemPicker name="picker4" label="分隔符" columns={basicColumns} separator=", " />
+      <BizFormItemPicker name="picker5" label="分隔符" columns={basicColumns} separator=", " />
     </DemoForm>
   );
 }
